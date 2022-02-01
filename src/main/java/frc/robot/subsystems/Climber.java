fbf85c74399de76955339b4258602bdaf9b3a3a2
SmellyRakes <95726258+Smellyrakes@users.noreply.github.com>
2022-02-01 18:08:56 -0500

    separating intake class from processor

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,21 +1,16 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+//climber subsystem
+//Seth Anderson, Francesco Canossi
 
 package frc.robot.subsystems;
 
 import static frc.robot.Constants.*;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import static frc.robot.Constants.DrivetrainConstants.*;
-
 import edu.wpi.first.wpilibj.XboxController;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
