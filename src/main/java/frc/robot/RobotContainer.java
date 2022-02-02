7966f9d78416a7eb12acc9a3725bbbcc83597968
froopy090 <97410685+froopy090@users.noreply.github.com>
2022-02-01 19:04:37 -0500

    quick changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -98,7 +98,7 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {
+  private void configureButtonBindings(){
     
     //intake
     
