2851b662f86fb7d346cb20754aa1c7cd7317d0b8
Thenewchicken55 <97483512+Thenewchicken55@users.noreply.github.com>
2022-02-08 17:30:43 -0500

    idk glitch fixed

--- src/main/java/frc/robot/commands/KillAutoCommand.java
+++ src/main/java/frc/robot/commands/KillAutoCommand.java
@@ -16,27 +16,6 @@ public class KillAutoCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
-<<<<<<< HEAD
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    if(killAuto == false){
-    killAuto = true;
-    } else
-    {
-    killAuto = false;
-    }
-   //if kill button clicked execute
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-=======
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -56,7 +35,7 @@ public class KillAutoCommand extends CommandBase {
 
   // Returns true when the command should end.
   @Override
->>>>>>> a57699f452e8005d9e4e4b784f3ec84fb3ff3cc5
+
   public boolean isFinished() {
     return true;
   }
